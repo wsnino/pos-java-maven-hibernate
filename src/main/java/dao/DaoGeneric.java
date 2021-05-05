5fe5d7f528103b89316a169a520a37ec37677677
marcelo pereira <wsnino.contato@gmail.com>
2021-05-05 10:41:40 -0300

    Dao Generic

--- src/main/java/dao/DaoGeneric.java
+++ src/main/java/dao/DaoGeneric.java
@@ -1,5 +1,18 @@
 package dao;
 
-public class DaoGeneric {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
+import posjavamavenhibernate.HibernateUtil;
+
+public class DaoGeneric<E> {
+
+	private EntityManager entityManager = HibernateUtil.getEntityManager();
+	
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+	}
 }
--- src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -2,10 +2,24 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 
 	@Test
 	public void testeHibernateUtil() { 
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setIdade(45);
+		pessoa.setLogin("teste");
+		pessoa.setNome("Marcelo Pereira");
+		pessoa.setSenha("123");
+		pessoa.setSobrenome("Nascimento");
+		pessoa.setEmail("wsnino.contato@gmail.com");
+		
+		daoGeneric.salvar(pessoa);
 	}
 }
